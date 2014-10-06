af50fe91f7b9431217c1a800a08d4169f5b45617
Scully87 <scully87.ts@googlemail.com>
2014-10-06 19:18:51 +0100

    filtering by tag

--- Bookmark_Manager/server.rb
+++ Bookmark_Manager/server.rb
@@ -6,6 +6,7 @@ env = ENV["RACK_ENV"] || "development"
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link'
+require './lib/tag'
  # this needs to be done after datamapper is initialised
 
 # After declaring your models, you should finalise them
@@ -16,5 +17,21 @@ DataMapper.auto_upgrade!
 
 get '/' do
   @links = Link.all
+  erb :index
+end
+
+post '/links' do
+url = params["url"]
+title = params["title"]
+tags = params["tags"].split(" ").map do |tag|
+Tag.first_or_create(:text => tag)
+end
+Link.create(:url => url, :title => title, :tags => tags)
+redirect to ('/')
+end
+
+get '/tags/:text' do
+  tag = Tag.first(:text => params[:text])
+  @links = tag ? tag.links : []
   erb :index
 end
