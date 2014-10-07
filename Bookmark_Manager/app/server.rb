de5b87069ee0567092132d256b48a3ad62dda6be
Scully87 <scully87.ts@googlemail.com>
2014-10-07 12:54:57 +0100

    cleaned up codebase

--- Bookmark_Manager/app/server.rb
+++ Bookmark_Manager/app/server.rb
@@ -3,25 +3,11 @@ require 'data_mapper'
 require './lib/link'
 require './lib/tag'
 require './lib/user'
+require_relative 'helpers/application'
+require_relative 'data_mapper_setup'
 
 enable :sessions
 set :session_secret, 'super secret'
-
-env = ENV["RACK_ENV"] || "development"
-# we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
-
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
-
- # this needs to be done after datamapper is initialised
-
-# After declaring your models, you should finalise them
-DataMapper.finalize
-
-# However, the database tables don't exist yet. Let's tell datamapper to create them
-DataMapper.auto_upgrade!
-
-	enable :sessions
-	set :session_secret, 'super secret'
 
 	get '/' do
 	  @links = Link.all
@@ -53,10 +39,4 @@ DataMapper.auto_upgrade!
 	  			  :password => params[:password])
 	  session[:user_id] = user.id
 	  redirect to('/')
-	end
-
-	helpers do 
-		def current_user
-		  @current_user ||=User.get(session[:user_id]) if session[:user_id]
-		end
 	end
